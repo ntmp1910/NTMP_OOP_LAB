76c187d90639559d623b3825d295f73e61ed579b
ntmp1910 <146692128+ntmp1910@users.noreply.github.com>
2024-03-29 23:30:07 +0700

    Lab02

---
+++ Lab02/AIMS/src/DigitalVideoDisc.java
@@ -0,0 +1,51 @@
+import java.lang.String;
+import java.util.Scanner;
+public class DigitalVideoDisc{
+    public String title;
+    public String category;
+    public String director;
+    public int length;
+    public double cost;
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+    public DigitalVideoDisc(String category, String title, double cost) {
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+    public DigitalVideoDisc(String director, String category, String title, double cost) {
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+    public DigitalVideoDisc(String title, String category, String director, int length, double cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public String getDirector() {
+        return director;
+    }
+    public int getLength() {
+        return length;
+    }
+    public double getCost() {
+        return cost;
+    }
+}
+
+
+
+
+
+
