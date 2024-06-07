d40eb7c2dd284653da02e6a07914a685ca0f323d
ntmp1910 <146692128+ntmp1910@users.noreply.github.com>
2024-06-07 17:37:02 +0700

    lab5

---
+++ Lab5/Painter/src/Painter.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Painter extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlfile = new FXMLLoader(PainterController.class.getResource("Painter.fxml"));
+        Scene scene = new Scene(fxmlfile.load());
+        stage.setTitle("Painter");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
